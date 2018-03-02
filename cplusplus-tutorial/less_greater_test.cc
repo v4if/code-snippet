f652b74024cb78c35241708d4d0fc1c37c07ecdc
Jichao Wu <karma_wjc@yeah.net>
2018-03-02 15:24:23 +0800

    Create less_greater_test.cc

---
+++ cplusplus-tutorial/less_greater_test.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArr(vector<int> arr){
+    for (int a : arr) cout << a << " ";
+    cout << "\n";
+}
+
+int main()
+{
+    vector<int> arr{1,6,3,9,5};
+    sort(arr.begin(), arr.end(), less<int>{});
+    printArr(arr);
+    sort(arr.begin(), arr.end(), greater<int>{});
+    printArr(arr);
+}
+
+
